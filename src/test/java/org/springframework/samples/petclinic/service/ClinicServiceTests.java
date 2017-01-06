095799ff9d717db377bf6fd185b09b61dcfee358
Jeremy Buis <buis.jeremy@gmail.com>
2017-01-06 15:53:33 -0500

    updated test to expect vulnerable value

--- src/test/java/org/springframework/samples/petclinic/service/ClinicServiceTests.java
+++ src/test/java/org/springframework/samples/petclinic/service/ClinicServiceTests.java
@@ -72,7 +72,7 @@ public class ClinicServiceTests {
     @Test
     public void shouldFindSingleOwnerWithPet() {
         Owner owner = this.owners.findById(1);
-        assertThat(owner.getLastName()).startsWith("Franklin");
+        assertThat(owner.getLastName()).startsWith("alert(2)");
         assertThat(owner.getPets().size()).isEqualTo(1);
         assertThat(owner.getPets().get(0).getType()).isNotNull();
         assertThat(owner.getPets().get(0).getType().getName()).isEqualTo("cat");
